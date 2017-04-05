bb035c93fb636546a724d74ce2af6ff352d4ea86
Fikri Habibie <fikri.habibie.fh@gmail.com>
2017-04-05 09:44:53 +0700

    Update faktorial.java

--- faktorial.java
+++ faktorial.java
@@ -13,30 +13,30 @@ public class Rpl2 {
         // TODO code application logic here
         System.out.println("Eko Pramono (103510281)");
         System.out.println("");
-        int l = 1;
+        int x = 1;
         int tingkat = 10;
         int n = 12/2;
         for(int i=1;i<=n;i++){
         for(int k=n;k>i;k--){
         System.out.print(" ");
         }
-        for(int j=1;j<=l;j++){
+        for(int j=1;j<=x;j++){
         System.out.print("+");
         }
-        l+=2;
+        x+=2;
 
         System.out.println("");
         }
-        l -= 4;
+        x -= 4;
 
         for(int i=(n-1);i>=1;i--){
         for(int k=i;k<=(n-1);k++){
         System.out.print(" ");
            }
-        for(int j=l;j>=1;j--){
+        for(int j=x;j>=1;j--){
         System.out.print("+");
         }
-        l-=2;
+        x-=2;
 
         System.out.println("");
         }
